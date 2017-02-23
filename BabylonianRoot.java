0aa49457e495bcfa33861f167f3ea58f6eee6917
mbrefka <mckaylabrefka@dyn-169-226-64-198.wireless.albany.edu>
2017-02-22 19:50:39 -0500

    attempt of recursive loop

--- BabylonianRoot.java
+++ BabylonianRoot.java
@@ -3,27 +3,41 @@ public class BabylonianRoot
 {
   private float squareRoot;
   private float guess;
+  private float original;
   private float outcome;
   private float last;
   private float answer;
   
-  public static void main(String [] args) {
-    Scanner keyboard= new Scanner(System.in);
-    float squareRoot = keyboard.next
+  public static void main(String [] args) 
+  {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("What would you like the square root of?");
+    float squareRoot = keyboard.nextFloat();
+    System.out.println("What do you guess the square root is?");
+    float guess= keyboard.nextFloat();
   }
+  
+  public static float sqrt(float squareRoot)
+  {
+    last = original;
+    original = (1/2)*(guess + (squareRoot/guess));
+    
+    if (last == outcome) {
+      return last;
+    }
+    else {
+      return (1/2)*(last + (squareRoot/last));
+    }
+    System.out.println(sqrt(4)); 
+  }
+}
   
   
   
   
   /*
-    System.out.println("What would you like the squareroot of?");
     System.out.println(squareRoot);
     System.out.println(guess);
-  }
-    float squareRoot = keyboard.nextLine();
-    float guess= keyboard.nextLine();
-    
-  }
   */
   
   
