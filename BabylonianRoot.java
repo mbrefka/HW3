7a79e76d347bca63c284e89787595ef60a83757d
mbrefka <mckaylabrefka@Mckaylas-MacBook-Pro.local>
2017-02-24 20:44:39 -0500

    remove extra method

--- BabylonianRoot.java
+++ BabylonianRoot.java
@@ -1,58 +1,56 @@
 import java.util.Scanner;
 public class BabylonianRoot
 {
-    private double squareRoot;
-    private double guess;
-    private double lastGuess;
-    private double newGuess;
-    private double error;
-    private double answer;
+    private double squareRoot; //number under the radical
+    private double guess; //first guess input
+    private double lastGuess; //place holder for input
+    private double newGuess; //guess from babylonian output
+    private double error; //allowed error
     Scanner keyboard = new Scanner(System.in);
     
-  private void getRoot()
-  {
-    System.out.println("What would you like the square root of?");
-    squareRoot = keyboard.nextDouble();
+    private void setRoot() {
+      System.out.println("What would you like the square root of?");
+      squareRoot = keyboard.nextDouble();
+    }
     
-  }
-  
-  private void guessValue() {
-    System.out.println("What do you guess the square root is?");
-    guess= keyboard.nextDouble();
-    System.out.println("How much error do you allow?");
-    error= keyboard.nextDouble();
-  }
+    private void guessValue() {
+      System.out.println("What do you guess the square root is?");
+      guess= keyboard.nextDouble();
     
-  private void setGuess()
+      System.out.println("How much error do you allow?");
+      error= keyboard.nextDouble();
+    }
+    
+    private void setGuess() //saves guess in placeholder, puts value through babylonian, saves as guess
   {
     lastGuess=guess;
-    newGuess=(guess+(squareRoot/guess))/2;
+    newGuess = (guess+(squareRoot/guess))/2;
     guess=newGuess;
-  }
-  
-  public double sqrt(double squareRoot, double guess, double error) {
-    if ((guess*guess)-squareRoot <= error)
-      return guess;
-    else 
-      br.setGuess();
-      return sqrt(squareRoot, guess, error);
     
- 
+  }
+    
+    public void sqrt() //squares guess value, subtracts squareroot. if less than error, prints output. else, runs methods again. 
+  {
+    if ((guess*guess-squareRoot) < error){
+      System.out.println((guess+(squareRoot/guess))/2);
+  }
+    else {
+      this.setGuess();
+      this.sqrt();
+    }
   }
     
   public static void main(String [] args) 
   {
     BabylonianRoot br = new BabylonianRoot();
-    br.getRoot();
+    br.setRoot();
     br.guessValue();
-    
-    
+    br.sqrt();
+  
   }
+  
 }
   
-  /*
-    System.out.println(squareRoot);
-    System.out.println(guess);
-  */
+  
   
   
